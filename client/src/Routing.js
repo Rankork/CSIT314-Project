4158f1c9493b8f8bb8f6e252ef8d7b8eeeb63b48
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2023-05-08 18:24:46 +1000

    Slight Padding update to taskselection page
    Rating page template draft complete
    Page Routing has also been updated.

--- client/src/Routing.js
+++ client/src/Routing.js
@@ -7,10 +7,14 @@ import ClientHome from "./_Client/pages/Home/home";
 import ClientMember from "./_Client/pages/Membership/membership";
 import TaskAllocation from "./_Client/pages/TaskAllocation/taskallocatoin";
 import TaskSelection from "./_Client/pages/TaskSelection/taskselection";
+import ClientRating from "./_Client/pages/Rating/rating";
+//TODO : import ClientReport from "./_Client/pages/Report/report";
 // Professional Pages
 import ProfessionalHome from "./_Professional/pages/Home/home";
 import ProfessionalMember from "./_Professional/pages/Membership/membership";
-
+//TODO : import ProfessionalTaskSelection form "./_Professional/pages/TaskSelection/taskselection";
+//TODO : import ProfessionalRating from "./_Professional/pages/Rating/rating";
+//TODO : import ProfessionalReport from "./_Professional/pages/Report/report";
 const Routing = () => {
   return (
     <Router>
@@ -26,13 +30,13 @@ const Routing = () => {
           path="/client/task-allocation"
           element={<TaskAllocation />}
         />
-
         <Route
           exact
           path="/client/task-selection"
           element={<TaskSelection />}
         />
-
+        <Route exact path="/client/rating" element={<ClientRating />} />
+        {/*TODO : <Route exact path ="/client/report" element={<ClientReport/>}>*/}
         {/*professional routes*/}
         <Route exact path="/professional" element={<ProfessionalHome />} />
         <Route
@@ -40,6 +44,9 @@ const Routing = () => {
           path="/professional/membership"
           element={<ProfessionalMember />}
         />
+        {/*TODO : <Route exact path ="/professional/task-selection" element={<ProfessionalTaskSelection/>}>*/}
+        {/*TODO : <Route exact path="/professional/rating" element={<ProfessionalTaskSelection />} />*/}
+        {/*TODO : <Route exact path="/professional/report" element={<ProfessionalReport/>} />*/}
       </Routes>
     </Router>
   );
---
+++ client/src/_Client/pages/Rating/rating.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./rating.css";
+import Header from "../../../Components/Header/Header";
+import Footer from "../../../Components/Footer/Footer";
+
+const Rating = () => {
+  return (
+    <div className="client-rating">
+      <Header />
+      <h1>Rating Page</h1>
+      <Footer />
+      <div className="client-rating-table">
+        <table>
+          <th>Professional Name</th>
+          <th>Trade</th>
+          <th>Rating</th>
+          <th>Feed back</th>
+          <tbody>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tbody>
+        </table>
+      </div>
+      <div className="NextPage-btn-container">
+        <button className="NextPage-btn">Next Page</button>
+      </div>
+    </div>
+  );
+};
+
+export default Rating;
