c833eb163476ef6df34d45c9e323cfac418e3bad
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2023-05-09 13:08:47 +1000

    Bug fix

--- client/src/Components/LoginPage/LoginPage.jsx
+++ client/src/Components/LoginPage/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Axios from "axios";
 import "./login-page.css";
 
