8f8f9789777f175ead672724579c5fc1629dad6c
Nathan, H <88916522+nHunter0@users.noreply.github.com>
2023-05-12 12:50:33 +1000

    Router update and report page complete

---
+++ client/src/_Professional/pages/Report/report.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../../../Components/Footer/Footer";
+import Header from "../../pages/ProfessionalHeader/Header";
+import "./report.css";
+
+const Report = () => {
+  return (
+    <div className="professional-report-page">
+      <Header />
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Report;
